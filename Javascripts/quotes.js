313cf66ebcf5316d575954aa14f405ac0c39a66b
Michael Lindstrom <michaellindstr@gmail.com>
2017-07-17 16:07:31 -0500

    changed quote bar

--- Javascripts/quotes.js
+++ Javascripts/quotes.js
@@ -1,8 +1,8 @@
-let quotes = ["Your family only loves you because they have to.", "You only get one body and you've already ruined it.", "Before you can love someone else you have to learn to love yourself so there's no chance of that hapening.", "Some people just aren't going to find love.", "I would love you if you were pizza.", "The first step towards failure is trying."]
+let quotes = ["Your family only loves you because they have to.", "You only get one body and you've already ruined it.", "Before you can love someone else you have to learn to love yourself so there's no chance of that hapening.", "Some people just aren't going to find love.", "I would love you if you were pizza.", "The first step towards failure is trying.", "'Expectations are resentments in the making. I have learned to have no or low expectations on things that are out of my control (everything). This way, I'm usually pleasantly surprised.' - Anne Lamott", "Listen to your heart. When you hear it stop beating, seek medical attention."]
 
 let postArea = document.getElementById("quotes");
 
 setInterval(function() {
   let randomNum = Math.floor(Math.random()*6);
-  postArea.innerHTML = `<p>${quotes[randomNum]}</p>`;
-}, 3000);
+  postArea.innerHTML = `<p>Inspirational Quote: ${quotes[randomNum]}</p>`;
+}, 4000);
